2a6268ee5a22f5335dfac8d422dd7d90db44ecf9
Daniel Velasquez <danieleduardovelasquezavila@gmail.com>
2023-09-19 11:10:44 -0600

    se paso la pagina de inicio de Sesion al formato para usar react

--- practicas/src/App.js
+++ practicas/src/App.js
@@ -4,8 +4,6 @@ import { PrimerComponente } from './components/PrimerComponente';
 function App() {
   return (
     <div className='App'>
-      <img src="https://plataformacii.ingenieria.usac.edu.gt/images/logo_fi.png" id=""></img>
-      <h1>Inicio de Sesion</h1>
       <PrimerComponente></PrimerComponente>
     </div>
   );
--- practicas/src/components/PrimerComponente.js
+++ practicas/src/components/PrimerComponente.js
@@ -7,12 +7,15 @@ export const PrimerComponente = () => {
     }
 
   return (
-    <div>
-        <p>Ingrese su Usuario</p>
-        <input placeholder='hello'></input>
-        <p>Ingrese su contraseña</p>
-        <input placeholder='Cada dia mas programador'></input><br></br><br></br>
-        <button onClick={PruebaFunciones}>Inicio de Sesion</button>
-    </div>
+    <form className='IngresoSes'>
+      <img id='logoUsac' src='https://plataformacii.ingenieria.usac.edu.gt/images/logo_fi.png'></img>
+      <p id='InSes'>INICIAR SESIÓN INGENIERIA USAC</p>
+      <input type='text' placeholder='CUI / REGISTRO ACADEMICO / REGISTRO PERSONAL' name='usuario'></input><br></br><br></br>
+      <input type='password' name='constraseña' placeholder='Contraseña'></input><br></br><br></br>
+
+      <input type='submit' value="Iniciar Sesion" id='btnIniSes'></input>
+      <a href='' id='forPass'><h4>Olvido su contraseña?</h4></a>
+      <a href='' id='forPass'><h4>No tiene usuario? Click</h4></a>
+    </form>
   )
 }
