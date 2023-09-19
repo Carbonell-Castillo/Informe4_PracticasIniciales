fc21773340c02336238721f1166986c1731add83
Daniel Velasquez <danieleduardovelasquezavila@gmail.com>
2023-09-18 20:11:32 -0600

    Se Comenzaron a crear los componentes de la pagina

--- practicas/src/App.js
+++ practicas/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import { PrimerComponente } from './components/PrimerComponente';
 
 function App() {
   return (
     <div className='App'>
+      <img src="https://plataformacii.ingenieria.usac.edu.gt/images/logo_fi.png" id=""></img>
       <h1>Inicio de Sesion</h1>
+      <PrimerComponente></PrimerComponente>
     </div>
   );
 }
---
+++ practicas/src/components/PrimerComponente.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const PrimerComponente = () => {
+
+    const PruebaFunciones = () => {
+
+    }
+
+  return (
+    <div>
+        <p>Ingrese su Usuario</p>
+        <input placeholder='hello'></input>
+        <p>Ingrese su contraseña</p>
+        <input placeholder='Cada dia mas programador'></input><br></br><br></br>
+        <button onClick={PruebaFunciones}>Inicio de Sesion</button>
+    </div>
+  )
+}
